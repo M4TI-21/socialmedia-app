92bce5acdfaf517e0392d57232feaebb18af90bb
Mateusz <mateuszhann05@gmail.com>
2023-10-03 11:18:01 +0200

    welocme page

--- sm-frontend/src/components/welcomePage/welcomePage.js
+++ sm-frontend/src/components/welcomePage/welcomePage.js
@@ -1,28 +1,30 @@
 import {Navbar, Container, Nav} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
+import logo from "../../images/sigmaLogo.png";
+import './welcomePageStyle.css';
 
 export default function WelcomePage() {
 
   return (
     <div className="page">
-        <div className="navbar">
-        <Navbar className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand>
-                    <img alt="" src="../../images/sigmaLogo.png" width="30" className="d-inline-block align-top"/>
-                    <p>Sigma</p>
-                </Navbar.Brand>
-                <Navbar.Collapse>
-                    <Nav>
-                        <Nav.Link><Link to="/">Sign in</Link></Nav.Link>
-                        <Nav.Link><Link to="/register">Register</Link></Nav.Link>
-                        <Nav.Link href="https://github.com/">Git</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        </div>
+        <Container className="">
+            <Navbar expand="lg" className="navbar" sticky="top" inverse collapseOnSelect>
+                <Container>
+                    <Navbar.Brand>
+                        <img alt="logo" src={logo} width="50"/>
+                        <p>Sigma</p>
+                    </Navbar.Brand>
+                    <Navbar.Collapse>
+                        <Nav className="nav d-flex flex-row justify-content-evenly">
+                            <Nav.Link className="navLink" href="https://github.com/">GIT</Nav.Link>
+                            <Nav.Link className="navLink"><Link className="navLink" to="/">SIGN IN</Link></Nav.Link>
+                            <Nav.Link className="navLink register"><Link className="navLink register" to="/register">REGISTER ACCOUNT</Link></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     </div>
   );
 }
