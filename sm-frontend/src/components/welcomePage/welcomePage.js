a03a716ea0f62fce4b6cfa7b6e85584ae1ff93b7
Mateusz <mateuszhann05@gmail.com>
2023-10-06 11:22:28 +0200

    welcome

--- sm-frontend/src/components/welcomePage/welcomePage.js
+++ sm-frontend/src/components/welcomePage/welcomePage.js
@@ -1,30 +1,19 @@
-import {Navbar, Container, Nav} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from "react-router-dom";
-import logo from "../../images/sigmaLogo.png";
-import './welcomePageStyle.css';
+import React from "react";
+import NavbarComp from "./elems/navbar";
 
 export default function WelcomePage() {
-
   return (
     <div className="page">
-        <Container className="">
-            <Navbar expand="lg" className="navbar" sticky="top" inverse collapseOnSelect>
-                <Container>
-                    <Navbar.Brand>
-                        <img alt="logo" src={logo} width="50"/>
-                        <p>Sigma</p>
-                    </Navbar.Brand>
-                    <Navbar.Collapse>
-                        <Nav className="nav d-flex flex-row justify-content-evenly">
-                            <Nav.Link className="navLink" href="https://github.com/">GIT</Nav.Link>
-                            <Nav.Link className="navLink"><Link className="navLink" to="/">SIGN IN</Link></Nav.Link>
-                            <Nav.Link className="navLink register"><Link className="navLink register" to="/register">REGISTER ACCOUNT</Link></Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </Container>
+        <NavbarComp />
+        <div className="contentArea">
+            <div className="quoteArea">
+                <p className="quote">Imagination <br /> rules <br />the World.</p>
+                <p className="signature">Napoleon Bonaparte</p>
+            </div>
+            <div className="">
+                <p>Login right now!</p>
+            </div>
+        </div>
     </div>
   );
 }
